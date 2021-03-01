fda13f12eb6fdf3b5ddc82b47c9fd74a1288e1cc
Dulat-Akan <astana7777777@gmail.com>
2021-03-01 15:44:20 -0800

    latest

--- controllers/subscribersCore.js
+++ controllers/subscribersCore.js
@@ -16,6 +16,7 @@ module.exports = function(io){
 
                    socket.join(data.deviceid);
                    var amount = parseInt(data.amount);
+                   var swithFamous = data.swithFamous;
 
                    multiple_db.query('SELECT * FROM appParams WHERE id = ?;', [1], function (error, results, fields) {
 
@@ -39,13 +40,22 @@ module.exports = function(io){
                        var FirstCount = videoPrice - percent;
                        var SecondCount = FirstCount / parseFloat(results[0].min_pay_rate);
                        var ThirstCount = parseInt(SecondCount);
+                       var NewCheckingAmount = 0;
+                       var countOfSubscribers = 0;
+                       if(swithFamous == true){
+                          NewCheckingAmount = amount + (results[0].famousPrice * ThirstCount);
+                          countOfSubscribers = ThirstCount * HelpLibrary.rangeSubscribers(parseInt(results[0].minUsersSubscribersHighRate),parseInt(results[0].platformMaxSubscribersHighRate));
+                       }else{
+                         NewCheckingAmount = amount;
+                         countOfSubscribers = ThirstCount * HelpLibrary.rangeSubscribers(parseInt(results[0].minUsersSubscribers),parseInt(results[0].platformMaxSubscribers));
+                       }
 
-                       var countOfSubscribers = ThirstCount * HelpLibrary.rangeSubscribers(parseInt(results[0].minUsersSubscribers),parseInt(results[0].platformMaxSubscribers));
+
 
                        var readableData = HelpLibrary.readableNumber(countOfSubscribers);
                        //console.log(countOfSubscribers);
 
-                       io.sockets.in(data.deviceid).emit('subscriberCore', cryptLibrary.encrypt({subscribersResult:readableData,originalNumber:countOfSubscribers,countOfBloggers:ThirstCount}));
+                       io.sockets.in(data.deviceid).emit('subscriberCore', cryptLibrary.encrypt({subscribersResult:readableData,originalNumber:countOfSubscribers,countOfBloggers:ThirstCount,NewCheckingAmount:NewCheckingAmount}));
 
                      }else{
                        io.sockets.in(data.deviceid).emit('subscriberCore', cryptLibrary.encrypt({status: 'false'}));
